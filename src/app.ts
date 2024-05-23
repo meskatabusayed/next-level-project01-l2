d47f41a6e8f8332f2c5ba88dbe6395179caa4bbd
meskatabusayed <meskatabusayed@gmail.com>
2024-05-24 00:40:18 +0530

    create operation successfully

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import cors from 'cors';
+import { StudentRoutes } from './modules/students/student.route';
 
 const app = express();
+
+//parser
+app.use(cors())
+app.use(express.json())
+
+//application routes
+app.use("/api/v1/students" , StudentRoutes);
 
 
 app.get('/', (req: Request, res: Response) => {
---
+++ src/modules/students/student.model.ts
@@ -0,0 +1,104 @@
+import { Schema, model } from 'mongoose';
+import { UserName, Guardian, LocalGuardian, Student} from './student.interface';
+
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuradianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: {
+    type : userNameSchema,
+    required : true
+  },
+  gender: {
+    type : String,
+    enum : ['male', 'female'],
+    required : true
+  } ,
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: 
+  {
+    type : String,
+    enum : ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddres: { type: String, required: true },
+  guardian: {
+    type : guardianSchema,
+    required : true
+  },
+  localGuardian: {
+    type : localGuradianSchema,
+    required : true
+  },
+  profileImg: { type: String },
+  isActive: {
+    type : String,
+    enum : ['active', 'blocked'],
+    default : 'active'
+  },
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
