6330e4d8f4f3a7411168b5cce9dadbaab7ec0b22
meskatabusayed <meskatabusayed@gmail.com>
2024-05-28 22:39:55 +0530

    validation update

--- src/modules/academicSemesters/academicSemester.services.ts
+++ src/modules/academicSemesters/academicSemester.services.ts
@@ -1,9 +1,23 @@
 
-import { TAcademicSemesterCode } from "./academicSemester.interface";
+import {  TAcedemicSemester } from "./academicSemester.interface";
 import { AcademicSemester } from "./academicSemester.model";
 
 
-const createAcademicSemesterInDb = async (payload : TAcademicSemesterCode) => {
+const createAcademicSemesterInDb = async (payload : TAcedemicSemester) => {
+
+    type TAcedemicSemesterNameCodeMapper = {
+        [key : string] : string;
+    }
+
+    const academicSemesterNameCodeMapper : TAcedemicSemesterNameCodeMapper = {
+        Autunm  : "01",
+        summar  : "02", 
+        Fall : "03"
+    }
+
+    if(academicSemesterNameCodeMapper[payload.name] !== payload.code){
+        throw new Error("Invalid Semester");
+    }
     const result = AcademicSemester.create(payload);
     return result;
 }
